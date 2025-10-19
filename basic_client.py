b1175768f00e6733375bdb7a90db6a18458f307d
aceprime80 <aceprime80@gmail.com>
2025-10-19 02:00:04 -0500

    short client (not finished)

---
+++ basic_client.py
@@ -0,0 +1,13 @@
+import requests
+import sys
+dst_arg = sys.argv[1]
+dst = "http://localhost:8000/upload-image"
+if dst_arg != "local":
+    dst = dst_arg
+path = "test_images/skyline.png"
+if len(sys.argv) == 3:
+    path = sys.argv[2]
+file_type = path.split(".")[1]
+file_binary_data = open(path, "rb").read()
+response = requests.post(url=dst, headers={"Content-Type":f'image/{file_type}'}, data=file_binary_data)
+print(response)
